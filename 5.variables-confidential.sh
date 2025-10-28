af7657fc1f4ea5b15add5fe496aa77e6337256df
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-10-28 19:28:04 +0530

    variables in shell script

---
+++ 5.variables-confidential.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script demonstrates the use of variables in bash about confidential information
+echo "Please enter your username:"
+read USERNAME
+echo "Please enter your password:"
+read  PASSWORD
+echo "Username is: $USERNAME"
+echo "Password is: $PASSWORD"
