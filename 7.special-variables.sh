d5de056fdf936768a81c16e1660cb26705d7b6ee
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-10-28 19:51:04 +0530

    variables in shell script

---
+++ 7.special-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script demonstrates the use of variables in bash some special variables
+echo "Script Name: $0"
+echo "First Argument: $1"
+echo "Second Argument: $2"
+echo "Total Number of Arguments: $#"
+echo "All Arguments: $*"
