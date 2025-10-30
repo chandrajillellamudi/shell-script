eff70b5e52df5d4f3b8bfa30f8ee572ce83da03a
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-10-30 06:45:57 +0530

    rough

--- rough.sh
+++ rough.sh
@@ -2,3 +2,14 @@
 # A rough example script
 NAME=Chandra
 echo "Hello, $NAME!"
+
+A=$1
+B=$2
+SUM=$((A + B))
+echo "The sum of $A and $B is $SUM"
+SUB=$((A - B))
+echo "The difference of $A and $B is $SUB"
+MUL=$((A * B))
+echo "The product of $A and $B is $MUL"
+DIV=$((A / B))
+echo "The quotient of $A and $B is $DIV"
