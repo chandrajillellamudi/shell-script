fd6a1a9e12647efdb77e6a7a595a1f48ea71291f
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-10-30 16:40:24 +0530

    conditions

--- 9.conditions.sh
+++ 9.conditions.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #Conditions in Bash
 var=$1
-
-if [$var -gt 1]
+if [ $var -gt 1 ]
 then
 echo "The variable is positive number"
-elif [$var -lt 1]
+elif [ $var -lt 1 ]
 then
 echo "The variable is negative number"
 else
