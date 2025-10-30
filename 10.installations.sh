894b766e5610340b95383747c6908d25c75e801b
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-10-30 16:45:25 +0530

    conditions

--- 10.installations.sh
+++ 10.installations.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 # Install MySQL
+ID=$(id-u)
+if [ $ID -ne 0 ]; then
+  echo "Please run as root user"
+  exit 1
+else
+  echo "Running as root user"       
+fi
 dnf install mysql -y
+
+echo "exit status"
