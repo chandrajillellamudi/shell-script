9f480822f0f3997440365861995c78d62fff43e3
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-11-04 10:24:12 +0530

    loops

--- loops.sh
+++ loops.sh
@@ -11,3 +11,14 @@ for fruit in "${fruits[@]}"
 do
   echo "$fruit"
 done
+
+#Installation of multiple packages using a loop
+for i in ${packages[@]}
+do
+    dnf install $i -y
+    if [ $? -eq 0 ]; then
+        echo "$i already installed.. skipping"
+        else
+        echo "Installation of $i successful"
+    fi
+done
