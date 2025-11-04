d46fbcaf48573fbc527e3f4eb2f5a1827f07438a
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-11-04 10:46:03 +0530

    loops

--- loops.sh
+++ loops.sh
@@ -4,11 +4,10 @@ for i in $@
 do
 echo "package name: $i"
 dnf install $i -y
-    if [ $? -ne 0 ];
-     then
-        echo -e"$i already installed.. "\e[33m"skipping"\e[0m"
-        else
-
-        echo -e"$i installed "\e[32m"successful"\e[0m"
-    fi
+   if [ $? -ne 0 ]; then
+     echo "$i installation failed"
+     exit 1
+   else
+     echo -e"$i already installed.. "\e[33m"skipping"\e[0m"
+   fi
 done
