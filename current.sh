4b1281449c2fb0b244c75931a78f4b0d35645a26
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-11-04 12:07:30 +0530

    changes

--- current.sh
+++ current.sh
@@ -4,6 +4,6 @@ Name="Chandra"
 echo "Value of Name  in current script is: $Name"
 echo "process id of current script is: $$"
 
-./other.sh
+source ./other.sh
 echo "Variable value after calling other script: $Name"
 
