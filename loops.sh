e65a767f9d4098f78798cef447eb27e4f45773c3
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-11-04 10:49:04 +0530

    loops

--- loops.sh
+++ loops.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 #Installation of multiple packages using a loop
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+if [ $ID -ne 0];
+then
+  echo -e "${R}Please run as root user${N}"
+    exit 1
+else
+  echo -e "${G}Running as root user${N}"
+fi
 for i in $@
 do
-echo "package name: $i"
+echo -e "${G}package name: $i${N}"
 dnf install $i -y
-   if [ $? -ne 0 ]; then
-     echo "$i installation failed"
-     exit 1
-   else
-     echo -e"$i already installed.. "\e[33m"skipping"\e[0m"
-   fi
+if [ $? -ne 0 ]; then
+  echo -e "${R}$i installation failed${N}"
+  exit 1
+else
+  echo -e "${G}$i installation successful${N}"
+fi
 done
